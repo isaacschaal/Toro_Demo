a272acb8c3f4560fbba75c2167303436f3691906
Isaac Schaal <isaac.h.schaal@gmail.com>
2020-03-22 17:15:14 -0700

    changed timer

--- main_timer.py
+++ main_timer.py
@@ -29,10 +29,10 @@ def main_mint():
                             universal_newlines=True)
     print (process.stdout)
 
-# Schedule the minting to happen everyday at 07:00
+# Schedule the minting to happen everyday at 08:00
 # and the new artworks to be generated everyday at 09:00
 schedule.every().day.at("09:00").do(main_generate)
-schedule.every().day.at("07:00").do(main_mint)
+schedule.every().day.at("08:00").do(main_mint)
 
 # Check for scheduled events every minute
 while True:
